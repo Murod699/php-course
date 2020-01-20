546dfb25bbd3af4336d51fa1b90947596247fb0f
Murodjon <murodbek.0338@gmail.com>
2020-01-20 11:57:51 +0500

    Second lesson

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+    //keyword function_name (param1,param2, param3)
+    function getNowDate() {
+        
+        return date('d.m.Y'); 
+    }
+
+    //Get time
+    function getNowTime() {
+        
+        return date('H:i:s'); 
+    }
+
+    // echo getNowTime();
+
+    // function add($a, $b) {
+    //     return $a+$b;
+    // }
+    function add($a, $b) {
+        echo $a+$b;
+    }
+
+    //echo add(5, 10);
+    add(5, 10);
